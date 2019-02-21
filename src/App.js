8f4fef5195cb8ee4a46ec059c3d6818465730088
Juanjo Arranz <juanjoarranzales@gmail.com>
2019-02-21 18:54:25 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Header from './components/layout/Header';
+import Sidebar from './components/layout/Sidebar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+
+const App = () => {
+
+    return (
+        <Router>
+            <div>
+                <Header />
+                <Sidebar />
+                <div className='rightContentContainer'>
+
+                    <Switch>
+                        <Route path='/' component={Home} exact={true}/>
+                    </Switch>
+
+                </div>
+            </div>
+        </Router>
+    );
+};
+
+
+export default App;
---
+++ src/components/layout/Sidebar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+const Sidebar = () => {
+
+    return (
+        <div className={'leftNavContainer'}>
+            <Link to='/' className='navLink'>Home</Link>
+
+        </div>
+    );
+};
+
+export default Sidebar; 
