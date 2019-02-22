3dd62bce671f3b069910ce653f390def806b607f
Juanjo Arranz <juanjoarranzales@gmail.com>
2019-02-22 11:43:45 +0100

    Refactor Sidebar for easy management of Left Navigation Links

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,17 +1,32 @@
 import React from "react";
 import { NavLink } from 'react-router-dom';
 
-
 const Sidebar = () => {
+
+    const links = [
+        { name: 'Color', url: '/color' },
+        { name: 'Prompt', url: '/prompt' },
+        { name: 'Prop Viewer', url: '/props' },
+        { name: '404 Error', url: '/404Error' },
+    ];
+
+    let linksComponents = links.map( ( link, index ) => {
+        return (
+            <li key={index} className={'nav'}>
+                <NavLink className={'navLink'} activeClassName={'activeNavLink'}
+                    to={link.url} exact>{link.name}</NavLink>
+            </li>
+        );
+    } );
 
     return (
         <div className={'leftNavContainer'}>
-            <NavLink to='/' activeClassName='activeNavLink' className='navLink' exact>Home</NavLink><br/>
-            <NavLink to='/prompt' activeClassName='activeNavLink' className='navLink'>Prompt</NavLink><br/>
-            <NavLink to='/props' activeClassName='activeNavLink' className='navLink'>Props</NavLink><br/>
-            <NavLink to='/foo' activeClassName='activeNavLink' className='navLink'>Not Found</NavLink><br/>
+            <ul>
+                <li className='nav'><NavLink className={'navLink'} activeClassName={'activeNavLink'} to='/' exact>Home</NavLink></li>
+                {linksComponents}
+            </ul>
         </div>
     );
 };
 
-export default Sidebar; 
+export default Sidebar;
