a10c5fe66b9abb7f59837a0a0e42c05d147caff5
Juanjo Arranz <juanjoarranzales@gmail.com>
2019-02-23 15:47:06 +0100

    Added source-map to Webpack for debugging

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-
+    devtool: 'source-map',
     entry: path.join(__dirname, 'src', 'index.js'),
 
     context: path.resolve(__dirname),
